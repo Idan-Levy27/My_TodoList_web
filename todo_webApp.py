3a1db1ad8756fc9e14d43c51b8ab0b09de509da1
Idan-Levy27 <73771891+Idan-Levy27@users.noreply.github.com>
2022-12-19 12:41:52 +0200

    Initial commit

---
+++ todo_webApp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ""
+
+st.title("My To-do List ")
+st.subheader("This is my web To-do list")
+st.write("My daily goals to increase my productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo.title()+'.', key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
